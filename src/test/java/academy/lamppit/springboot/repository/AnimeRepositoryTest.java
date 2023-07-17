9b4a24648d2cfb18e54500875fa8e5f857def832
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-17 09:35:57 -0300

    Teste de persistencia JPA

---
+++ src/test/java/academy/lamppit/springboot/repository/AnimeRepositoryTest.java
@@ -0,0 +1,32 @@
+package academy.lamppit.springboot.repository;
+
+import academy.lamppit.springboot.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@DisplayName("Tests for Anime Repository")
+class AnimeRepositoryTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save creates anime when Successful")
+    void save_PersistAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+
+    }
+
+    private Anime createAnime(){
+        return Anime.builder()
+                .name("Hajime no Ippo")
+                .build();
+    }
+}
