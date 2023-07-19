d2b7ab387fd724afba084aff703e520ef9eaf466
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-19 17:41:44 -0300

    Config Security | HttpSec&AutheManager

---
+++ src/main/java/academy/lamppit/springboot/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package academy.lamppit.springboot.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("PasswordEnconder: {}", passwordEncoder.encode("chave"));
+        auth.inMemoryAuthentication()
+                .withUser("julio")
+                .password(passwordEncoder.encode("julio"))
+                .roles("USER","ADMIN")
+                .and()
+                .withUser("Teste")
+                .password(passwordEncoder.encode("teste"))
+                .roles("USER");
+
+
+    }
+}
